81bf0bb0834b6d70e3db4f23e8532e9e22d702fe
Stephen Hoekstra <shoekstra@schubergphilis.com>
2019-05-22 12:58:21 +0200

    Set "User-Agent" header
    Newer versions of the Cisco API require this to be one of "REST API
    Agent", "ASDM" or "CSM".
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- ciscoasa/ciscoasa.go
+++ ciscoasa/ciscoasa.go
@@ -145,6 +145,7 @@ func (c *Client) newRequest(method string, api string, v interface{}) (*http.Req
 
 	req.Close = true
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Set("User-Agent", "REST API Agent")
 	req.SetBasicAuth(c.username, c.password)
 
 	return req, nil
